3e575c785988877c70a7bcc5d30886d641c63e19
Eric Valois <eric.v@bulledev.com>
2018-01-19 11:32:17 -0500

    Update the plugin version to 1.3

--- ttfb-toolkit.php
+++ ttfb-toolkit.php
@@ -3,7 +3,7 @@
 Plugin Name:       TTFB Toolkit
 Plugin URI:        https://github.com/time-to-first-byte/ttfb-toolkit
 Description:       The TTFB Toolkit extends functionality to TTFB Themes, providing Font Awesome icons, alerts and more.
-Version:           1.2
+Version:           1.3
 Author:            TTFB
 */
 
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) exit();
 
 
 // Toolkit version
-define( 'TTFB_TOOLKIT_VERSION', '1.2' );
+define( 'TTFB_TOOLKIT_VERSION', '1.3' );
 
 // Toolkit root directory
 define( 'TTFB_TOOLKIT_DIR', trailingslashit( plugin_dir_path( __FILE__ ) ) );
